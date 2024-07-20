d344625b8c031b80a02b74d05b01307a9daad4de
ImAdityaa12 <adityagupta1291@gmail.com>
2024-07-21 00:33:47 +0530

    Animations

--- Animations/src/script.js
+++ Animations/src/script.js
@@ -1,31 +1,46 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+let time = Date.now();
+const clock = new THREE.Clock();
+const tick = () => {
+  //1st Method
+  //   let currentTime = Date.now();
+  //   let deltaTime = currentTime - time;
+  //   time = currentTime;
+  //2nd Method
+  const elaspsedTime = clock.getElapsedTime();
+  // Update objects
+  mesh.position.y = Math.sin(elaspsedTime);
+  // Render scene for each frame
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
